a1de76df3276839e032213bc2218946db8cf2628
Maria <scacciamaria19@gmail.com>
2025-10-10 14:11:35 -0400

    Changes to be committed: 	modified:   join.py

--- checkfile.py
+++ checkfile.py
@@ -1,14 +1,13 @@
 import pandas as pd
 
-input_path = r"C:\Users\10216172\OneDrive - State of Ohio\Desktop\MS_UC\WilliamsCoWells\WELL_TEST_WilliamsCo.rpt"
-output_path = r"C:\Users\10216172\OneDrive - State of Ohio\Desktop\MS_UC\WilliamsCoWells\WELL_TEST_WilliamsCo.csv"
-#Read the file
-file = pd.read_csv(input_path, delim_whitespace=True)
+#input_path = r"C:\Users\10216172\OneDrive - State of Ohio\Desktop\MS_UC\WilliamsCoWells\WELL_TEST_WilliamsCo.rpt"
+output_path = r"C:\Users\10216172\OneDrive - State of Ohio\Desktop\MS_UC\WilliamsCoWells\WELL_GROUT_WilliamsCo.csv"
 
-column_to_skip = ''  # Replace with the actual column name
+#Read the file
+#file = pd.read_csv(input_path, delim_whitespace=True)
 
 #Save new file
-file.to_csv(output_path, index=False)
+#file.to_csv(output_path, index=False)
 
 file_new = pd.read_csv(output_path)
 print("Complete")
--- join.py
+++ join.py
@@ -8,7 +8,7 @@ arcpy.CheckOutExtension("Spatial")
 
 #input data
 well_points = r"C:\Users\10216172\OneDrive - State of Ohio\Desktop\MS_UC\WilliamsCoWells\WellsWilliamsCo.gdb\WellLogs_Williams"
-geo_forms = r"C:\Users\10216172\OneDrive - State of Ohio\Desktop\MS_UC\WilliamsCoWells\GEO_FORM_WilliamsCo.txt"
+geo_forms = r"C:\Users\10216172\OneDrive - State of Ohio\Desktop\MS_UC\WilliamsCoWells\GEO_FORM_WilliamsCo.csv"
 
 #join the two tables based on a common field
 arcpy.management.JoinField(
