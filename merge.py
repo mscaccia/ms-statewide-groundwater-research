517d1cd697d3667e9c1ab0c319e2b33f4b9e167f
Maria <scacciamaria19@gmail.com>
2025-10-14 22:16:40 -0400

    modified:   merge.py

--- merge.py
+++ merge.py
@@ -3,3 +3,5 @@ import pandas as pd
 df1 = pd.read_csv(r"C:\Users\scacc\Desktop\MS_UC\Repositories\ms-statewide-groundwater-research\WilliamsCoWells\GEO_FORM_WilliamsCo.csv")
 df2 = pd.read_csv(r"C:\Users\scacc\Desktop\MS_UC\Repositories\ms-statewide-groundwater-research\WilliamsCoWells\Wells_Logs.csv")
 
+new_df2= df2[["WELL_LOG_NO", "LAT", "LON", "Head", "RASTERVALU", "NEW_COMP_ELEV"]].copy()
+print(new_df2)
